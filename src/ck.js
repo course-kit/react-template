cdf9846799596f5d56c7ca402c0b0f45aa16cf5c
Anthony Gore <anthony@vuejsdevelopers.com>
2022-04-06 09:59:50 +1000

    Update ck.js

--- src/ck.js
+++ src/ck.js
@@ -1,5 +1,8 @@
 import { CourseKitClient } from '@coursekit/client'
-const ck = new CourseKitClient({ schoolId: process.env.REACT_APP_SCHOOL_ID })
+const ck = new CourseKitClient({
+  schoolId: process.env.REACT_APP_SCHOOL_ID,
+  devMode: process.env.NODE_ENV === 'development'
+})
 
 const fetchUser = async () => {
   const { status, user } = await ck.loadUser()
